4bb787f6181c80db7b58a7abf8f1dd478dd4f18a
forgetSalary <lantsovandrew@gmail.com>
2023-03-26 16:54:32 +0300

    using "path"

--- src/pages/api/getPoints.ts
+++ src/pages/api/getPoints.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import directoryTree from "directory-tree";
+import path from 'path';
 import fs from 'fs'
 import { MapPoint } from '../../components/AppMap';
 import { serverPath } from './helpers';
@@ -12,12 +13,10 @@ export default function handler(
   res: NextApiResponse<GetPointsRes>
 ) {
   try{
-    const path = serverPath(".");
-    console.log("path: ", path);
-    const tree = directoryTree(path, {attributes:["type", "extension"]});
-    console.log(JSON.stringify(tree, null, " "));
-
-    const buf = fs.readFileSync('./data.json');
+    const dir = path.join(process.cwd(), 'public');
+    console.log("dir: ", dir);
+    
+    const buf = fs.readFileSync(dir + '/data.json');
     const fileContent = buf.toString('utf8');
     const resObj: GetPointsRes = JSON.parse(fileContent)
     res.status(200).send(resObj);
